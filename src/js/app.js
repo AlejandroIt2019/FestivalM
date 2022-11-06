659779ca6d5035215344ae5dbd5463a575d5bd4c
Alejandro <italicdev@gmail.com>
2022-11-06 18:33:34 -0300

    Scroll v1

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,22 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function iniciarApp() {
     crearGaleria();
+    scrollNav();
+}
+//scroll
+function scrollNav() {
+    const enlaces = document.querySelectorAll('.navegacion-principal a');
+
+    enlaces.forEach(enlaces => {
+        enlaces.addEventListener('click', function (e) {
+            e.preventDefault();
+            
+            const seccionScroll = e.target.attributes.href.value;
+            const seccion = document.querySelector(seccionScroll);
+            seccion.scrollIntoView({ behavior: 'smooth'});
+        });
+    })
+
 }
 
 function crearGaleria() {
@@ -41,11 +57,23 @@ function mostrarImagen(id){
         const overlay = document.createElement('DIV');
         overlay.appendChild(imagen);
         overlay.classList.add('overlay');
+        overlay.onclick = function (){
+            const body = document.querySelector('body');
+            body.appendChild(overlay)
+            
+            overlay.remove();
+        }
 
         //cerrar ventana
         const cerrarModal = document.createElement('P');
         cerrarModal.textContent = 'X';
         cerrarModal.classList.add('btn-cerrar');
+        cerrarModal.onclick = function () {
+            const body = document.querySelector('body');
+            body.appendChild(overlay)
+            
+            overlay.remove();
+        }
         overlay.appendChild(cerrarModal);
 
 
@@ -53,5 +81,6 @@ function mostrarImagen(id){
         //añadirlo al html
         const body = document.querySelector('body');
         body.appendChild(overlay)
+        body.classList.add('fijar-body');
 }
 
